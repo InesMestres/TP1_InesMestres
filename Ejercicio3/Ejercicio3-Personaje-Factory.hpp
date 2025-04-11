cf8df9957a8140c6bbe7612f16df3aa852d794a2
InesMestres <imestres@udesa.edu.ar>
2025-04-11 11:59:05 -0300

    FUnciones de crear personajes

--- Ejercicio3/Ejercicio3-Personaje-Factory.hpp
+++ Ejercicio3/Ejercicio3-Personaje-Factory.hpp
@@ -5,50 +5,50 @@
 
 class PersonajeFactory {
     private:
+        //LLamadas a crear personajes
+        //Magos:
+        static std::shared_ptr<Personaje> crear_hechicero();
+        static std::shared_ptr<Personaje> crear_conjurador();
+        static std::shared_ptr<Personaje> crear_brujo();
+        static std::shared_ptr<Personaje> crear_nigromante();
+        //Guerreros:
+        static std::shared_ptr<Personaje> crear_barbaro();
+        static std::shared_ptr<Personaje> crear_paladin();
+        static std::shared_ptr<Personaje> crear_caballero();
+        static std::shared_ptr<Personaje> crear_mercenario();
+        static std::shared_ptr<Personaje> crear_gladiador();
 
-    //LLamadas a crear personajes
-    //Magos:
-    static std::shared_ptr<Personaje> crear_hechicero();
-    static std::shared_ptr<Personaje> crear_conjurador();
-    static std::shared_ptr<Personaje> crear_nigromante();
-    static std::shared_ptr<Personaje> crear_nigromante();
-    //Guerreros:
-    static std::shared_ptr<Personaje> crear_barbaro();
-    static std::shared_ptr<Personaje> crear_paladin();
-    static std::shared_ptr<Personaje> crear_caballero();
-    static std::shared_ptr<Personaje> crear_mercenario();
-    static std::shared_ptr<Personaje> crear_gladiador();
+        //LLamadas a crear armas:
+        //Items mágicos:
+        static std::shared_ptr<Arma> crear_baston();
+        static std::shared_ptr<Arma> crear_libroDeHechizos();
+        static std::shared_ptr<Arma> crear_pocion();
+        static std::shared_ptr<Arma> crear_amuleto();
+        //Armas de combate:
+        static std::shared_ptr<Arma> crear_hachaSimple();
+        static std::shared_ptr<Arma> crear_hachaDoble();
+        static std::shared_ptr<Arma> crear_espada();
+        static std::shared_ptr<Arma> crear_lanza();
+        static std::shared_ptr<Arma> crear_garrote();
 
-    //LLamadas a crear armas:
-    //Items mágicos:
-    static std::shared_ptr<Arma> crear_baston();
-    static std::shared_ptr<Arma> crear_libroDeHechizos();
-    static std::shared_ptr<Arma> crear_pocion();
-    static std::shared_ptr<Arma> crear_amuleto();
-    //Armas de combate:
-    static std::shared_ptr<Arma> crear_hachaSimple();
-    static std::shared_ptr<Arma> crear_hachaDoble();
-    static std::shared_ptr<Arma> crear_espada();
-    static std::shared_ptr<Arma> crear_lanza();
-    static std::shared_ptr<Arma> crear_garrote();
-
-    //LLamadas a crear personajes_armados:
-    //Magos:
-    static std::shared_ptr<Personaje> crear_hechicero();
-    static std::shared_ptr<Personaje> crear_conjurador();
-    static std::shared_ptr<Personaje> crear_nigromante();
-    static std::shared_ptr<Personaje> crear_nigromante();
-    //Guerreros:
-    static std::shared_ptr<Personaje> crear_barbaro();
-    static std::shared_ptr<Personaje> crear_paladin();
-    static std::shared_ptr<Personaje> crear_caballero();
-    static std::shared_ptr<Personaje> crear_mercenario();
-    static std::shared_ptr<Personaje> crear_gladiador();
+        //LLamadas a crear personajes_armados:
+        //Magos:
+        static std::shared_ptr<Personaje> crear_hechicero();
+        static std::shared_ptr<Personaje> crear_conjurador();
+        static std::shared_ptr<Personaje> crear_nigromante();
+        static std::shared_ptr<Personaje> crear_nigromante();
+        //Guerreros:
+        static std::shared_ptr<Personaje> crear_barbaro();
+        static std::shared_ptr<Personaje> crear_paladin();
+        static std::shared_ptr<Personaje> crear_caballero();
+        static std::shared_ptr<Personaje> crear_mercenario();
+        static std::shared_ptr<Personaje> crear_gladiador();
 
     
     public: 
+        //Funciones para crear personaje, arma y personaje armado:  
         static std::shared_ptr<Personaje> crear_personaje(const std::string& tipo_personaje);
         static std::shared_ptr<Arma> crear_arma(const std::string& tipo_arma);
         static std::shared_ptr<Personaje> crear_personaje_armado(const std::string& tipo_personajeArmado);
 
-}
+};
--- Ejercicio3/Ejercicio3-PersonajeFactory.cpp
+++ Ejercicio3/Ejercicio3-PersonajeFactory.cpp
@@ -1,6 +1,5 @@
-#include "Eje"
-
 #include "Ejercicio3-Personaje-Factory.hpp"
+
 #include <memory>
 
 /*2.3. Teniendo en cuenta que un personaje “has-a” arma, es decir existe una
@@ -17,31 +16,82 @@ recibir un parámetro que indique que se quiere este tipo de objeto. Luego,
 basándose en conceptos de polimorfismo, esta función devolverá un puntero
 al objeto requerido.*/
 
-std::shared_ptr<Personaje> PersonajeFactory::crear_personaje(const std::string& tipo) {
-    if (tipo == "hechicero") {
+//Creador de personajes: 
+
+std::shared_ptr<Personaje> PersonajeFactory::crear_personaje(const std::string& tipo){
+    if(tipo == "hechicero" || tipo == "Hechicero"){
         return crear_hechicero();
-    } else if (tipo == "conjurador") {
+    }
+    if(tipo == "conjurador" || tipo == "Conjurador"){
         return crear_conjurador();
-    } else if (tipo == "brujo") {
+    }
+    if(tipo == "brujo" || tipo == "Brujo"){
         return crear_brujo();
-    } else if (tipo == "nigromante") {
+    }
+    if(tipo == "nigromante" || tipo == "Nigromante"){
         return crear_nigromante();
-    } else if (tipo == "barbaro") {
+    }   
+    if(tipo == "barbaro" || tipo == "Barbaro"){
         return crear_barbaro();
-    } else if (tipo == "paladin") {
+    }
+    if(tipo == "paladin" || tipo == "Paladin"){
         return crear_paladin();
-    } else if (tipo == "caballero") {
+    }
+    if(tipo == "caballero" || tipo == "Caballero"){
         return crear_caballero();
-    } else if (tipo == "mercenario") {
+    }
+    if(tipo == "mercenario" || tipo == "Mercenario"){
         return crear_mercenario();
-    } else if (tipo == "gladiador") {
+    }
+    if(tipo == "gladiador" || tipo == "Gladiador"){
         return crear_gladiador();
-    } else {
-        throw std::invalid_argument("Tipo de personaje desconocido: " + tipo);
+    }
+    else{
+        throw std::invalid_argument("El tipo de personaje" + tipo + " es desconocido");
     }
 }
 
-static std::shared_ptr<Personaje> crear_personaje_armado(const std::string& tipo);
+
+//Creador de armas:
+
+std::shared_ptr<Arma> PersonajeFactory::crear_arma(const std::string& tipo){
+    if(tipo == "baston" || tipo == "Baston"){
+        return crear_baston();
+    }
+    if(tipo == "libro de hechizos" || tipo == "Libro de Hechizos"){
+        return crear_libroDeHechizos();
+    }
+    if(tipo == "pocion" || tipo == "Pocion"){
+        return crear_pocion();
+    }
+    if(tipo == "amuleto" || tipo == "Amuleto"){
+        return crear_amuleto();
+    }
+    if(tipo == "hacha simple" || tipo == "Hacha Simple"){
+        return crear_hachaSimple();
+    }   
+    if(tipo == "hacha doble" || tipo == "Hacha Doble"){
+        return crear_hachaDoble();
+    }
+    if(tipo == "espada" || tipo == "Espada"){
+        return crear_espada();
+    }
+    if(tipo == "lanza" || tipo == "Lanza"){
+        return crear_lanza();
+    }
+    if(tipo == "garrote" || tipo == "Garrote"){
+        return crear_garrote();
+    }
+    else{
+        throw std::invalid_argument("El tipo de arma " + tipo + " es desconocida");
+    }
+}
+
+//Creador de personajes con armas: 
+
+static std::shared_ptr<Personaje> crear_personaje_armado(const std::string& tipo){
+    
+}
 
 std::shared_ptr<Personaje> PersonajeFactory::crear_hechicero() {
     return std::make_shared<hechicero>();
