6228061d03023e8737c95c4a3b8e846dbe46c3af
InesMestres <imestres@udesa.edu.ar>
2025-04-15 01:35:56 -0300

    Comentarios

--- Ejercicio1/Personajes.hpp
+++ Ejercicio1/Personajes.hpp
@@ -4,9 +4,26 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include "Armas.hpp"
 
-// interfaz de Personajes:
 
+/*
+Class Personaje: interfaz abstracta para representar personajes en el juego.  
+Define la base para los tipos de personajes: magos y guerreros. 
+Atributos:
+- std::string tipo_personaje: tipo de personaje (dentro de los magos y guerreros, hay varios personajes concretos)
+- std::vector<std::shared_ptr<Arma>> armas: vector que se inicializa como vacío y contiene las armas que se le asignan al personaje.
+- int vida_personaje: cantidad de vida que le queda al personaje. 
+Métodos: 
+- Personaje(std::string tipo, int vida): constructor del personaje
+- virtual void esta_presente() = 0 : método virtual puro que muestra la presencia del personaje en batalla.
+- std::string get_tipo_personaje() const: retorna el tipo de personaje
+- int get_vida(): retorna la vida actual del personaje. 
+- virtual void agregar_arma(std::shared_ptr<Arma> arma): agrega arma a vector de armas del personaje
+- virtual int perder_vida(int cantidad_vidas): resta vida al personaje segun daño que recibe.
+- void atacar_personaje(std::shared_ptr<Personaje> atacado): permite atacar a personaje en el juego, aplicando daño. 
+- virtual ~Personaje() {} : destructor virtual
+*/
 class Personaje
 {
 protected:
@@ -16,16 +33,33 @@ protected:
 
 public:
     Personaje(std::string tipo, int vida);
-    std::string get_tipo_personaje() const;
     virtual void esta_presente() = 0;
-    virtual ~Personaje() {}
-    virtual void agregar_arma(std::shared_ptr<Arma> arma);
+    std::string get_tipo_personaje() const;
     int get_vida();
+    virtual void agregar_arma(std::shared_ptr<Arma> arma);
     virtual int perder_vida(int cantidad_vidas);
     void atacar_personaje(std::shared_ptr<Personaje> atacado);
+    virtual ~Personaje() {}
 };
 
-// Clase abstracta: Magos
+
+
+
+/*
+Clase abstracta Magos, hereda de clase Personaje
+Arma la base de representación de personajes de tipo mago en el juego
+Atributos: 
+- int nivel_experiencia_mago: nivel de experiencia que tiene el mago
+- std::string especialidad_mago: la especialidad del tipo de mago presentado.
+- int energia_mago: la cantidad de energía que tiene el mago. 
+Métodos: 
+- mago(std::string tipo_mago, int nivel_experiencia_mago, std::string especialidad_mago, int energia_mago, int vida_mago) : Personaje(tipo_mago, vida_mago) {}: constructor de mago.
+- int get_nivel_experiencia_mago() const: retorna nivel de experiencia del mago.
+- std::string get_especialidad_mago() const: devuelve la especialidad del mago.
+- int get_energia_mago(): devuelve la energía que tiene el mago para afrontar combates. 
+- void descripcion_mago(): permite describir el mago. 
+*/
+
 
 class mago : public Personaje
 {
@@ -35,14 +69,19 @@ protected:
     int energia_mago;
 
 public:
-    mago(std::string tipo_mago, int nivel_experiencia_mago, std::string especialidad_mago, int energia_mago, int vida_mago) : Personaje(tipo_mago, vida_mago) {};
+    mago(std::string tipo_mago, int nivel_experiencia_mago, std::string especialidad_mago, int energia_mago, int vida_mago) : Personaje(tipo_mago, vida_mago), nivel_experiencia_mago(nivel_experiencia_mago), especialidad_mago(especialidad_mago), energia_mago(energia_mago) {};
     int get_nivel_experiencia_mago() const;
     std::string get_especialidad_mago() const;
     int get_energia_mago();
     void descripcion_mago();
 };
 
-// Tipos de magos: hechicero, conjurador, brujo y nigromante.
+
+/*Subclases concretas de la clase abstracta Mago
+Representan los tipos de magos concretos disponibles en el juego (hechicero, conjurador, brujo, nigromante).
+Cada uno posee atributos: tipo de mago, nivel de experiencia, especialidad mágica, energía y vida actual.ADJ_OFFSET_SINGLESHOT
+*/
+
 
 class hechicero : public mago
 {
@@ -68,7 +107,25 @@ public:
     nigromante() : mago("nigromante", 10, "Muerte", 6, 100) {}
 };
 
-// Clase Abstracta: guerreros
+
+
+
+
+
+/*
+Clase abstracta guerrero, hereda de clase Personaje
+Arma la base de representación de personajes de tipo guerrero en el juego
+Atributos: 
+- int nivel_experiencia_guerrero: nivel de experiencia  del guerrero.
+- std::string cualidad_guerrero: cualidad principal que posee este tipo de guerrero.
+- int energia_guerrero: la cantidad de energía que posee el guerrero. 
+Métodos: 
+- guerrero(std::string tipo_guerrero, int nivel_experiencia_guerrero, std::string especialidad_guerrero, int energia_guerrero, int vida_guerrero) : constructor que inicializa guerrero, y sus atributos y métodos heredados y propios.
+- int get_nivel_experiencia_guerrero() const: retorna el nivel de experiencia del guerrero.
+- std::string get_cualidad_guerrero() const: retorna la cualidad principal del guerrero.
+- int get_energia_guerrero(): retorna la energía que tiene el guerrero para afrontar combates. 
+- void descripcion_guerrero(): describe el guerrero por consola. 
+*/
 
 class guerrero : public Personaje
 {
@@ -78,14 +135,19 @@ protected:
     int energia_guerrero;
 
 public:
-    guerrero(std::string tipo_guerrero, int nivel_experiencia_guerrero, std::string especialidad_guerrero, int energia_guerrero, int vida_guerrero) : Personaje(tipo_guerrero, vida_guerrero);
+    guerrero(std::string tipo_guerrero, int nivel_experiencia_guerrero, std::string cualidad_guerrero, int energia_guerrero, int vida_guerrero) : Personaje(tipo_guerrero, vida_guerrero), nivel_experiencia_guerrero(nivel_experiencia_guerrero), cualidad_guerrero(cualidad_guerrero), energia_guerrero(energia_guerrero) {};
     int get_nivel_experiencia_guerrero() const;
     std::string get_cualidad_guerrero() const;
     int get_energia_guerrero();
     void descripcion_guerrero();
 };
 
-// Tipos de guerreros: barbaro, paladín, caballero, mercenario, gladiador
+
+
+/*Subclases concretas de la clase abstracta guerrero
+Representan los tipos de guerreros concretos disponibles en el juego (barbaro, paladín, caballero, mercenario, gladiador).
+Cada uno posee atributos: tipo de guerrero, nivel de experiencia, una cualidad del guerrero, energía y vida actual.
+*/
 
 class barbaro : public guerrero
 {
