2afb9fdd00eb67684fc57f0cefb24c3889c548bd
InesMestres <imestres@udesa.edu.ar>
2025-04-17 09:14:07 -0300

    Comentarios

--- Ejercicio1/Funciones-Arma.cpp
+++ Ejercicio1/Funciones-Arma.cpp
@@ -3,16 +3,15 @@
 #include <string>
 
 
+//FUNCIONES DE INTERFAZ ARMA:
 
-//FUNCIONES DE CASO BASE ARMA:
-
+//Constructor:
 Arma::Arma(const std::string& nombre): nombre(nombre){}
 
+//Devuelve el nombre del tipo de arma: 
 std:: string Arma::get_nombre() const{
     return nombre;
 }
-
-
 
 
 
@@ -20,80 +19,106 @@ std:: string Arma::get_nombre() const{
 
 //FUNCIONES DE CLASE ABSTRACTA ITEM MAGICO:
 
+//Constructor: 
 itemMagico::itemMagico(const std::string& nombre_itemMagico, const std::string& elemento_itemMagico, const std::string& rareza_itemMagico, const std::string& color_itemMagico, int destruccion_itemMagico): Arma(nombre_itemMagico), elemento_itemMagico(elemento_itemMagico), rareza_itemMagico(rareza_itemMagico), color_itemMagico(color_itemMagico), destruccion_itemMagico(destruccion_itemMagico){}
 ;
 
+//Devuelve elemento del item mágico:
 std::string itemMagico::get_elemento_ItemMagico() const{
     return elemento_itemMagico;
 }
 
+//Devuelve rareza del item mágico:
 std::string itemMagico::get_rareza_itemMagico() const{
     return rareza_itemMagico;
 }
 
+//Devuelve color del item mágico:
 std::string itemMagico::get_color_itemMagico() const{
     return color_itemMagico;
 }
 
+//Devuelve nivel de destrucción del item mágico:
 int itemMagico::get_destruccion_itemMagico() const{
     return destruccion_itemMagico;
 }
 
+//Permite imprimir datos del arma por consola para demostrar que está presente en el juego
 void itemMagico::usar_arma() {
     std::cout << "Usando el Item Mágico: " << nombre << std::endl;
     return;
 }
 
-
+//Destructor:
 itemMagico::~itemMagico(){}
 
+
+
 //-------------------------------//
+//SUBCLASES CONCRETAS DE CADA ITEM MAGICO:
 
-//FUNCIONES ESPECIFICAS DE CADA ITEM MAGICO:
 
+//BASTON:
 
-//Bastón:
+//Devuelve largo del bastón:
 int baston::get_largo() const{
     return largo;
 }
+//Devuelve el peso del bastón:
 int baston::get_peso() const{
     return peso;
 }
+//Imprime datos del bastón por la consola para demostrar que está presente:
 void baston::usar_arma(){
     std::cout << "Tipo Item Mágico: " << nombre << " - Elemento: " << elemento_itemMagico << " - Rareza: " << rareza_itemMagico << " - Color: " << color_itemMagico << "Peso: " << peso << " kg  - Largo: " << largo << " - Destrucción: " << destruccion_itemMagico << std::endl;
 }
 
-//Libro de hechizos:
+
+//LIBRO DE HECHIZOS:
+
+//Devuelve la cantidad de hojas que tiene el libro:
 int libroDeHechizos::get_cantidad_hojas() const{
     return cantidad_hojas;
 }
+//Devuelve el idioma del libro:
 std::string libroDeHechizos::get_idioma() const{
     return idioma;
 }
+//Imprime datos del libro por la consola para demostrar que está presente:
 void libroDeHechizos::usar_arma(){
     std::cout << "Tipo Item Mágico: " << nombre << " - Elemento: " << elemento_itemMagico << " - Rareza: " << rareza_itemMagico << " - Color: " << color_itemMagico << "Cantidad de Hojas: " << cantidad_hojas << "Idioma: " << idioma << " - Destrucción: " << destruccion_itemMagico << std::endl;
     return;
 }
 
-//Poción:
+
+//POCIÓN:
+
+//Devuelve el efecto que genera la poción:
 std::string pocion::get_efecto() const{
     return efecto;
 }
+//Retorna el ingrediente secreto de la poción: 
 std::string pocion::get_ingrediente_secreto() const{
     return ingrediente_secreto;
 }
+////Imprime datos de la poción por la consola para demostrar que está presente:
 void pocion::usar_arma(){
     std::cout << "Tipo Item Mágico: " << nombre << " - Elemento: " << elemento_itemMagico << " - Rareza: " << rareza_itemMagico << " - Color: " << color_itemMagico << " - Efecto: " << efecto << " - Ingrediente Secreto: " << ingrediente_secreto << " - Destrucción: " << destruccion_itemMagico << std::endl;
     return;
 }
 
-//Amuleto
+
+//AMULETO:
+
+//Devuelve el tipo de pierda del que está hecho el amuleto:
 std::string amuleto::get_tipo_piedra() const{
     return tipo_piedra;
 }
+//Retorna el poder específico del amuleto:
 std::string amuleto::get_poder() const{
     return poder;
 }
+////Imprime datos del amuleto por la consola para demostrar que está presente:
 void amuleto::usar_arma(){
     std::cout << "Tipo Item Mágico: " << nombre << " - Elemento: " << elemento_itemMagico << " - Rareza: " << rareza_itemMagico << " - Color: " << color_itemMagico << " - Tipo Piedra: " << tipo_piedra << " - Poder: " << poder << " - Destrucción: " << destruccion_itemMagico << std::endl;
     return;
@@ -106,30 +131,36 @@ void amuleto::usar_arma(){
 
 //FUNCIONES CLASE ABSTRACTA DE ARMA DE COMBATE
 
-
+//Constructor:
 armaDeCombate::armaDeCombate(const std::string& nombre_armaCombate, const std::string& material_armaCombate, const std::string& rareza_armaCombate, int destruccion_armaCombate, int precision_armaCombate): Arma(nombre_armaCombate), material_armaCombate(material_armaCombate), rareza_armaCombate(rareza_armaCombate), destruccion_armaCombate(destruccion_armaCombate), precision_armaCombate(precision_armaCombate) {}
 
+//Retorna el material del arma:
 std::string armaDeCombate::get_material_armaCombate() const{
     return material_armaCombate;
 }
 
+//Retorna la rareza del arma:
 std::string armaDeCombate::get_rareza_armaCombate() const{
     return rareza_armaCombate;
 }
 
+//Retorna la destrucción del arma:
 int armaDeCombate::get_destruccion_armaCombate() const{
     return destruccion_armaCombate;
 }
 
+//Retorna la precisión del arma:
 int armaDeCombate::get_precision_armaCombate() const{
     return precision_armaCombate;
 }
 
+//Imprime información de este tipo de arma por la consola para demostrar que está presente en el juego. 
 void armaDeCombate::usar_arma() {
     std::cout << "Usando el Arma de Combate: " << nombre << std::endl;
     return;
 }
 
+//Destructor:
 armaDeCombate::~armaDeCombate() {}
 
 
@@ -137,64 +168,83 @@ armaDeCombate::~armaDeCombate() {}
 
 //FUNCIONES ESPECIFICAS DE CADA ARMA DE COMBATE:
 
-//Hacha Simple:
+//HACHA SIMPLE:
 
+//Retorna el nivel de filo del hacha:
 int hachaSimple::get_nivel_filo() const{
     return nivel_filo;
 }
+//Retorna el peso del hacha:
 int hachaSimple::get_peso() const{
     return peso;
 }
+//Imprime datos generales y específicos de hacha para poder demostrar que está presente en el juego. 
 void hachaSimple:: usar_arma(){
     std::cout << "Tipo Arma de combate: " << nombre << " - Rareza: " << rareza_armaCombate << " - Material: " << material_armaCombate << " - Peso: " << peso << " kg - Nivel Filo: " << nivel_filo << " - Destrucción: " << destruccion_armaCombate << " - Precisión: " <<precision_armaCombate << std::endl;
     return;
 }
 
-//Hacha Doble: 
 
+//HACHA DOBLE: 
+
+//Retorna el nivel de filo del hacha:
 int hachaDoble::get_nivel_filo() const{
     return nivel_filo;
 }
+//Devuelve el peso del hacha:
 int hachaDoble::get_peso() const{
     return peso;
 }
+//Imprime datos generales y específicos de hacha para poder demostrar que está presente en el juego:
 void hachaDoble:: usar_arma(){
     std::cout << "Tipo Arma de combate: " << nombre << " - Rareza: " << rareza_armaCombate << " - Material: " << material_armaCombate << " - Peso: " << peso << " kg  - Nivel Filo: " << nivel_filo << " - Destrucción: " << destruccion_armaCombate << " - Precisión: " <<precision_armaCombate << std::endl;
     return;
 }
 
-//Espada:
 
+//ESPADA:
+
+//Devuelve la forma de la espada:
 std::string espada::get_forma() const{
     return forma;
 }
+//Retorna longitud de la espada:
 int espada::get_longitud() const{
     return longitud;
 }
+//Imprime datos generales y específicos de espada para poder demostrar que está presente en el juego. 
 void espada::usar_arma(){
     std::cout << "Tipo Arma de combate: " << nombre << " - Rareza: " << rareza_armaCombate << " - Material: " << material_armaCombate << " - Forma: " << forma << " -  Longitud: " << longitud << " mts  - Destrucción: " << destruccion_armaCombate << " - Precisión: " <<precision_armaCombate << std::endl;
     return;
 }
 
-//lanza:
+//LANZA:
+
+//Retorna la forma de la punta de la lanza: 
 std::string lanza::get_forma_punta() const{
     return forma_punta;
 }
+//Retorna el lugar de orígen de la lanza. 
 std::string lanza::get_lugar_origen() const{
     return lugar_origen;
 }
+////Imprime datos generales y específicos de lanza para poder demostrar que está presente en el juego. 
 void lanza::usar_arma(){
     std::cout << "Tipo Arma de combate: " << nombre << " - Rareza: " << rareza_armaCombate << " -  Lugar de Orígen: " << lugar_origen << " - Material: " << material_armaCombate << " - Forma de Punta: " << forma_punta  << " mts  - Destrucción: " << destruccion_armaCombate << " - Precisión: " <<precision_armaCombate << std::endl;
     return;
 }
 
-//Garrote:
+//GARROTE:
+
+//Retorna el uso principal del garrote:
 std::string garrote::get_uso() const{
     return uso;
 }
+//Retorna la época en la cual mas se utilizó el garrote:
 std::string garrote::get_epoca_utilizacion() const{
     return epoca_utilizacion;
 }
+//Imprime datos generales y específicos de garrote para poder demostrar que está presente en el juego. 
 void garrote::usar_arma(){
     std::cout << "Tipo Arma de combate: " << nombre << " - Rareza: " << rareza_armaCombate << " -  Uso Principal: " << uso << " - Epoca de uso: " << epoca_utilizacion  << " - Material: " << material_armaCombate << " - Destrucción: " << destruccion_armaCombate << " - Precisión: " <<precision_armaCombate << std::endl;
     return;
--- Ejercicio1/Funciones-Personaje.cpp
+++ Ejercicio1/Funciones-Personaje.cpp
@@ -2,31 +2,34 @@
 #include "Armas.hpp"
 #include <string>
 
-//CLASE BASE: PERSONAJES:
+//FUNCIONES DE INTERFAZ PERSONAJE
 
-
-//PERSONAJE FUNCIONES
-
+//Constructor personaje:
 Personaje::Personaje(const std::string& tipo, int vida) : tipo_personaje(tipo), vida_personaje(vida) {}
 
+//Retorna el tipo de personaje:
 std::string Personaje::get_tipo_personaje() const {
     return tipo_personaje;
 }
 
+//Añade un arma al vector de armas de personaje:
 void Personaje::agregar_arma(std::shared_ptr<Arma> arma){
     armas.push_back(arma);
 }   
 
+//Retorna cantidad de vida del personaje:
 int Personaje::get_vida(){
     return vida_personaje;
 }
 
+//Le resta vida a un personaje si pierde un combate:
 int Personaje::perder_vida(int cantidad_vidas){
     vida_personaje -= cantidad_vidas;
     if(vida_personaje <= 0){vida_personaje = 0;}
     return vida_personaje; 
 }
 
+//Permite a un personaje atacar a otro:
 void Personaje::atacar_personaje(std::shared_ptr<Personaje> atacado) {
     atacado->perder_vida(10);
     if (armas.size() > 0) {
@@ -39,80 +42,105 @@ void Personaje::atacar_personaje(std::shared_ptr<Personaje> atacado) {
 
 
 
-
-
+//----------------------------------------------------------------------//
 
 //FUNCIONES CLASE ABSTRACTA MAGO:
 
+//Constructor mago:
 mago::mago(std::string tipo_mago, int nivel_experiencia_mago, std::string especialidad_mago, int energia_mago, int vida_mago) : Personaje(tipo_mago, vida_mago), nivel_experiencia_mago(nivel_experiencia_mago), especialidad_mago(especialidad_mago), energia_mago(energia_mago) {}
 
+//Retorna el nivel de experiencia del mago:
 int mago::get_nivel_experiencia_mago() const{
     return nivel_experiencia_mago;
 }
 
+//Retorna la especialidad del mago:
 std::string mago::get_especialidad_mago() const{
     return especialidad_mago;
 }
 
+//Retorna la energia del mago:
 int mago::get_energia_mago(){
     return energia_mago;
 }
 
+//Avisa por consola que el mago es parte del juego. 
 void mago::esta_presente() {
     std::cout << "Esta presente un Mago: " << tipo_personaje << std::endl;
     return;
 }
 
+//Destructor de mago:
 mago::~mago() {}
 
 
+//---------------//
 
+//FUNCIONES PERSONALIZADAS DE CADA SUBCLASE CONCRETA DE MAGO:
 
-//FUNCIONES PERSONALIZADAS DE CADA TIPO DE MAGO:
+//SUBCLASEHECHICERO:
 
-//Hechicero: 
+//Retorna su vestimenta;
 std::string hechicero::get_vestimenta() const{
     return vestimenta;
 }
+//Retorna su gorro;
 std::string hechicero::get_gorro() const{
     return gorro;
 }
+//Avisa por consola que el hechicero es parte del juego
 void hechicero::esta_presente(){
     std::cout << "Tipo de mago: " << tipo_personaje << " - Nivel de experiencia: " << nivel_experiencia_mago << " - Especialización: " << especialidad_mago << "Vestimenta: " << vestimenta << " - Accesorio: " << gorro << " Energía: " << energia_mago << " - Vida: " << vida_personaje << std::endl;
     return;
 }
 
-//Conjurador:
+//SUBCLASE CONJURADOR:
+
+//Retorna un sinónimo de la palabra "conjurador":
 std::string conjurador::get_sinonimo() const{
     return sinonimo;
 }
+
+//Retorna el riesgo que corren los conjuradores:
 std::string conjurador::get_riesgo() const{
     return riesgo;
 }
+
+//Avisa por consola que el conjurador está presente en el juego:
 void conjurador::esta_presente(){
     std::cout << "Tipo de mago: " << tipo_personaje << " - Sinónimo: " << sinonimo << " - Nivel de experiencia: " << nivel_experiencia_mago << " - Especialización: " << especialidad_mago << " Energía: " << energia_mago << " - Vida: " << vida_personaje << " - Riesgo/Debilidad: " << riesgo << std::endl;
     return;
 }
 
-//Brujo:
+//SUBCLASE BRUJO:
+
+//Devuelve cualidad del brujo: 
 std::string brujo::get_cualidad() const{
     return cualidad;
 }
+//Devuelve la época de crecimiento de la Brujería": 
 std::string brujo::get_epoca() const{
     return epoca;
 }
+
+//Avisa que el brujo está presente en el juego. 
 void brujo::esta_presente(){
     std::cout << "Tipo de mago: " << tipo_personaje << " - Nivel de experiencia: " << nivel_experiencia_mago << " - Especialización: " << especialidad_mago << " - Cualidad Brujo: " << cualidad << " - Época de orígen: " << epoca << " - Energía: " << energia_mago << " - Vida: " << vida_personaje << std::endl;
     return;
 }
 
-//Nigromante:
+
+//SUBCLASE NIGROMANTE:
+
+//Devuelve una cualidad del nigromante:
 std::string nigromante::get_cualidad() const{
     return cualidad;
 }
+//Devuelve el país del que provee este tipo de brujería:
 std::string nigromante::get_pais() const{
     return pais;
 }
+//Avisa por consola que está presente un nigromante en el juego:
 void nigromante::esta_presente(){
     std::cout << "Tipo de mago: " << tipo_personaje << " - Nivel de experiencia: " << nivel_experiencia_mago << " - Especialización: " << especialidad_mago << " - País de origen: " << pais << " - Cualidad: " << cualidad << " - Energía: " << energia_mago << " - Vida: " << vida_personaje << std::endl;
     return;
@@ -120,94 +148,116 @@ void nigromante::esta_presente(){
 
 
 
-
-
-
-//GUERREROS: 
+//----------------------------------------------------------------------//
 
 
 //FUNCIONES CLASE ABSTRACTA GUERRERO:
 
+//Constructor: 
 guerrero::guerrero(std::string tipo_guerrero, int nivel_experiencia_guerrero, std::string cualidad_guerrero, int energia_guerrero, int vida_guerrero) : Personaje(tipo_guerrero, vida_guerrero), nivel_experiencia_guerrero(nivel_experiencia_guerrero), cualidad_guerrero(cualidad_guerrero), energia_guerrero(energia_guerrero){}
 
+//Retorna el nivel de experiencia del guerrero:
 int guerrero::get_nivel_experiencia_guerrero() const{
     return nivel_experiencia_guerrero;
 }
-
+//Devuelve cualidad del guerrero: 
 std::string guerrero::get_cualidad_guerrero() const{
     return cualidad_guerrero;
 }
-
+//Retorna el nivel de energía actual del guerrero: 
 int guerrero::get_energia_guerrero(){
     return energia_guerrero;
 }
-
+//Imprime datos por consola para avisar que está presente un guerrero:
 void guerrero::esta_presente(){
     std::cout << "Esta presente un Guerrero: " << tipo_personaje << std::endl;
     return;
 }
-
+//Destructor:
 guerrero::~guerrero() {}
 
 
-//FUNCIONES PERSONALIZADAS DE CADA TIPO DE GUERRERO: 
+//------------------//
 
-//Barbaro:
+
+//FUNCIONES PERSONALIZADAS DE CADA SUBCLASE CONCRETA DE GUERRERO: 
+
+//BÁRBARO:
+
+//Retorna el nombre del rey de los bárbaros:
 std::string barbaro::get_nombre_rey() const{
     return nombre_rey;
 }
+//Devuelve el nivel de ira que tiene el guerrero: 
 int barbaro::get_ira() const{
     return ira;
 }
-
+//Demuestra imprimiendo información por consola, que está presente este tipo de guerrero:
 void barbaro::esta_presente(){
     std::cout << "Tipo Guerrero: " << tipo_personaje  << " - Nombre: " << nombre_rey << " - Nivel de Experiencia: " << nivel_experiencia_guerrero << " - Principal Cualidad: " << cualidad_guerrero << " - Nivel Energía: " << energia_guerrero << " Vida: " << vida_personaje << " - Nivel ira: " << ira << std::endl;
     return;
 }
 
-//Paladin:
+//PALADIN:
+
+//Devuelve la prioridad de un paladín: 
 std::string paladin::get_prioridad() const{
     return prioridad;
 }
+//Retorna el arma secreta de un paladín:
 std::string paladin::get_acceso() const{
     return acceso;
 }
+//Avisa que un paladín está presente en el juego
 void paladin::esta_presente(){
     std::cout << "Tipo Guerrero: " << tipo_personaje << " - Nivel de Experiencia: " << nivel_experiencia_guerrero << " - Principal Cualidad: " << cualidad_guerrero << " - Prioridad: " << prioridad << " - Arma Secreta: " << acceso << " - Nivel Energía: " << energia_guerrero << " Vida: " << vida_personaje << std::endl;
     return;
 }
 
-//Caballero:
+
+//CABALLERO:
+
+//Devuelve el nombre del caballo que monta el caballero: 
 std::string caballero::get_nombre_caballo() const{
     return nombre_caballo;
 }
+//Retorna el material de la armadura del caballero: 
 std::string caballero::get_material_armadura() const{
     return material_armadura;
 }
+//Avisa que el caballero está presente en el juego. 
 void caballero::esta_presente(){
     std::cout << "Tipo Guerrero: " << tipo_personaje << " - Nivel de Experiencia: " << nivel_experiencia_guerrero << " - Principal Cualidad: " << cualidad_guerrero << " - Nombre Caballo: " << nombre_caballo << " - Material Armadura: " << material_armadura << " - Nivel Energía: " << energia_guerrero << " Vida: " << vida_personaje << std::endl;
     return;
 }
 
-//Mercenario:
+//MERCENARIO:
+
+//Devuelve la motivación del mercenario:
 std::string mercenario::get_motivacion() const{
     return motivacion;
 }
+//Devuelve el contratante del mercenario: 
 std::string mercenario::get_contratante() const{
     return contratante;
 }
+//Imprime datos por consola para demostrar que un mercenario está presente en el juego:
 void mercenario::esta_presente(){
     std::cout << "Tipo Guerrero: " << tipo_personaje << " - Nivel de Experiencia: " << nivel_experiencia_guerrero << " - Principal Cualidad: " << cualidad_guerrero << " - Motivación: " << motivacion << " - Contratante: " << contratante << " - Nivel Energía: " << energia_guerrero << " Vida: " << vida_personaje << std::endl;
     return;
 }
 
-//Gladiador:
+//GLADIADOR:
+
+//Devuelve la cantidad de monstruos que venció el gladiador:
 int gladiador::get_monstruos_vencidos() const{
     return monstruos_vencidos;
 }
+//Devuelve la ciudad donde vive el gladiador:
 std::string gladiador::get_ciudad() const{
     return ciudad;
 }
+//El gladiador se muestra presente imprimiendo su información por consola: 
 void gladiador::esta_presente(){
     std::cout << "Tipo Guerrero: " << tipo_personaje << " - Nivel de Experiencia: " << nivel_experiencia_guerrero << " - Monstruos Vencidos: " << monstruos_vencidos << " - Principal Cualidad: " << cualidad_guerrero  << " - Ciudad: " << ciudad << " - Nivel Energía: " << energia_guerrero << " Vida: " << vida_personaje << std::endl;
     return;
